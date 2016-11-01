efc97f4c149b90f08ed1ab18f15abbef4d6944cc
Amy Hwang <amyhwang1218@gmail.com>
2016-11-01 07:12:15 -0700

    add remove feature

--- js/scripts.js
+++ js/scripts.js
@@ -15,26 +15,36 @@ $(document).ready(function() {
 
     var newToDo = new ToDo(inputtedtodo, inputteddetail, inputteddeadline);
 
-    $("ul#to-do").append("<li><span class='toDo'>" + newToDo.todo + "  " + "<button type='button'>completed</button></span></li>");
-
-    $(".toDo").last().click(function() {
+    var openToDoDetail = function() {
+      // show the todo details
       $("#show-to-do h2").text(newToDo.todo);
       $(".todo").text(newToDo.todo);
       $(".detail").text(newToDo.detail);
       $(".deadline").text(newToDo.deadline);
       $("#show-to-do").show();
+    }
+    var hideToDoDetail = function() {
+      // close the todo details
+      $("#show-to-do").fadeOut();
+    }
+
+    // add new to do element
+    var $newToDo = $("<li><span class='toDo'>" + newToDo.todo + "  " + "<button class='complete'>completed</button></span></li>");
+    $("ul#to-do").append($newToDo);
+
+    $newToDo.find(".complete").on('click', function() {
+      // hide the to do item if the user clicks OK.
+      var complete = confirm("Press okay to complete the task: " + newToDo.todo);
+      if (complete == true) {
+        $(this).closest("li").fadeOut();
+        hideToDoDetail();
+      }
     });
+    $(".toDo").last().click(openToDoDetail);
 
     $("#new-todo").val("");
     $("#new-detail").val("");
     $("#new-deadline").val("");
 
-    $("button").click(function() {
-      var complete = confirm("Press okay when the task is completed.")
-      if (complete == true) {
-        $("toDo").fadeOut();
-        $("this").fadeOut();
-      }
-    });
   })
 })
